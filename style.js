5c91b2b6dbc9f7f9e76fbcf5cc22a28968337996
Richard Walter <richardwalter515@gmail.com>
2020-10-04 15:27:42 -0400

    created JS file and date/time elements

---
+++ style.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    //display date at top
+    var date = moment().format('LL');
+    $('#currentDay').html(date);
+
+    //variable for current hour
+    var currentHour = moment().format('h');
+    console.log('currentHour:', currentHour)
+
+    //variables for time block hours
+    // var nineAM = 
+    // var tenAm = 
+    // var elevenAm = 
+    // var twelvePm = 
+    // var onePm = 
+    // var twoPm = 
+    // var threePm = 
+    // var fourPm = 
+    // var fivePm = 
+    //if time block hour > current hour, turn green
+
+    //if time block hour < current hour, turn red
+
+    //else turn grey
+
+    //on click of save button, save text info to storage
+        
+    // }
+});
